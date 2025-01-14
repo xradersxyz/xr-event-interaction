91cc5c7c5a93adc28d40a1320873f0a3c63856a7
kevin.ryu <7721251+net2me@users.noreply.github.com>
2025-01-14 12:22:26 +0900

    add pauseable

--- contracts/bnb/EventInteraction.sol
+++ contracts/bnb/EventInteraction.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
 
-contract EventInteraction is Initializable, OwnableUpgradeable {
+contract EventInteraction is Initializable, OwnableUpgradeable,PausableUpgradeable {
     mapping(address => uint256) private lastAttendanceTime;
 
     /**
@@ -25,17 +26,17 @@ contract EventInteraction is Initializable, OwnableUpgradeable {
         __Ownable_init(initialOwner);
     }
 
-    function attendance() public {
+    function attendance() public whenNotPaused {
         require(canAttend(), "Already attended today");
         lastAttendanceTime[msg.sender] = getCurrentTime();
         emit Attendance(msg.sender);
     }
 
-    function getLastAttendanceTime(address user) public view returns (uint256) {
+    function getLastAttendanceTime(address user) public view whenNotPaused returns (uint256) {
         return lastAttendanceTime[user];
     }
 
-    function canAttend() private view returns (bool) {
+    function canAttend() private view whenNotPaused returns (bool) {
         uint256 currentDay = getCurrentTime();
         uint256 lastAttendedDate = lastAttendanceTime[msg.sender];
         return lastAttendedDate != currentDay;
@@ -44,5 +45,13 @@ contract EventInteraction is Initializable, OwnableUpgradeable {
     function getCurrentTime() private view returns (uint256) {
         // 1 day = 86400 seconds
         return block.timestamp / 86400;
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
     }
 }
